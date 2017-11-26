cfc333ef0eb0559fa1da15b75dbcdf03bf6d7b0d
Vincent Lee <vincent.h.lee@outlook.com>
2017-11-26 14:04:16 -0800

    Migrate changes.

--- ZenithClient/ClientApp/app/components/app/app.component.ts
+++ ZenithClient/ClientApp/app/components/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
 @Component({
-    selector: 'app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private _httpService: Http) { }
+  apiValues: string[] = [];
+  title = 'app';
+  
+  ngOnInit() {
+    /*this._httpService.get('/api/values').subscribe(values => {
+        this.apiValues = values.json() as string[];
+    });*/
+  }
 }
