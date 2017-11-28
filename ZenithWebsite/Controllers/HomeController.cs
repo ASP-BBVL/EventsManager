3b10ef6e3a9aac41e00b6bdcebdc181c8e12fd14
Bryn Beaudry <babeaudry@gmail.com>
2017-11-27 16:54:26 -0800

    Front page is similar to older project

--- ZenithWebsite/Controllers/HomeController.cs
+++ ZenithWebsite/Controllers/HomeController.cs
@@ -5,14 +5,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ZenithWebsite.Models;
+using ZenithWebsite.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ZenithWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var dates = new Dictionary<DateTime, List<Event>>();
+            //TODO get current date => Week
+            var today = DateTime.Now;
+            if (today.DayOfWeek != DayOfWeek.Monday)
+            {
+                int delta = 0;
+                if (today.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    delta = -6;
+                }
+                else
+                {
+                    delta = DayOfWeek.Monday - today.DayOfWeek;
+                }
+                today = today.AddDays(delta);
+            }
+            System.Diagnostics.Debug.WriteLine(today.ToShortDateString());
+            while (today.DayOfWeek != DayOfWeek.Sunday)
+            {
+                var events = _context.Events.Include(e => e.Activity).Where(e => e.StartDate.Day == today.Day && e.StartDate.Month == today.Month && e.StartDate.Year == today.Year && e.IsActive).ToList();
+                dates.Add(today, events);
+                today = today.AddDays(1);
+            }
+            return View(dates);
         }
 
         public IActionResult About()
